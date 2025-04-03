94ecaaa7a673faa176701468df4a21925ed49329
George Kouros <george.kouros.ece@gmail.com>
2025-04-03 15:08:40 +0200

    Fix build_ext script

--- build_ext.sh
+++ build_ext.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+set -e
+export CPATH=$CONDA_PREFIX/include:$CPATH
+export CUDA_HOME=$CONDA_PREFIX
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 cd freqencoder/; rm -rf build; python setup.py build_ext --inplace; mkdir _ext; mv *.so _ext; cd ..;
 cd gridencoder/; rm -rf build; python setup.py build_ext --inplace; mkdir _ext; mv *.so _ext; cd ..;
 cd hashencoder/; rm -rf build; python setup.py build_ext --inplace; mkdir _ext; mv *.so _ext; cd ..;
-cd shencoder/; rm -rf build; python setup.py build_ext --inplace; mkdir _ext; mv *.so _ext; cd ..;
+cd shencoder/;   rm -rf build; python setup.py build_ext --inplace; mkdir _ext; mv *.so _ext; cd ..;
 cd raymarching/; rm -rf build; python setup.py build_ext --inplace; mkdir _ext; mv *.so _ext; cd ..;
